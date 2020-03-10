3932bddbcbf58e3758ef69298c7c2ad18a015df5
mwinter13 <44899466+mwinter13@users.noreply.github.com>
2020-03-09 18:34:46 -0600

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+This is a classic game of hangman with ASCII graphics.  90% of the code is copied, line by line (not copy and paste) from http://inventwithpython.com/invent4thed/chapter8.html
+
+I modified some of the code, then debuugged it on my own.
---
+++ hangman.py
@@ -0,0 +1,117 @@
+import random
+HANGMAN_PICS = ['''
+ +---+
+     |
+     |
+     |
+    ===''','''
+ +---+
+ O   |
+     |
+     |
+    ===''','''
+ +---+
+ O   |
+ |   |
+     |
+    ===''','''
+ +---+
+ O   |
+/|   |
+     |
+    ===''','''
+ +---+
+ O   |
+/|\  |
+     |
+    ===''','''
+ +---+
+ O   |
+/|\  |
+/    |
+    ===''','''
+ +---+
+ O   |
+/|\  |
+/ \  |
+    ===''']
+
+words = 'pizza burrito pie orange chips cake'.split()
+
+#this function returns a random word from a list that you enter
+def getRandomWord(wordList):
+    randomIndex = random.randint(0, len(wordList)-1)
+    return wordList[randomIndex]
+
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_PICS[len(missedLetters)])
+    print()
+    print('missed letters:', end=' ')
+    for letter in missedLetters:
+        print(letter, end=', ')
+    print()
+    blanks = '_' * len(secretWord)
+#replace blanks with secret word   
+    for i in range(len(secretWord)):
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:] 
+#mess with the above line when finished.  Still a little unclear how this works- 
+#probable answer- "blanks" is a continually updated variable
+
+    for letter in blanks:
+        print(letter, end=' ')
+    print()
+
+def getGuess(alreadyGuessed):
+    #returns the letter the player guessed.  Makes sure the player entered a single letter and
+    #not something else
+    while True:
+        print('Guess a letter...')
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('You can only guess one letter at a time')
+        elif guess in alreadyGuessed:
+            print('You already guessed this.  Choose again')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('You can only guess letters.  Choose again')
+        else:
+            return guess
+
+def playAgain():
+    print('Do you want to play again? Yes or No?')
+    return input().lower().startswith('y')
+    
+print('WELCOME TO HANGMAN!')
+missedLetters = ''
+correctLetters = ''
+secretWord = getRandomWord(words)
+gameIsDone = False
+
+while True:
+    displayBoard(missedLetters, correctLetters, secretWord)
+    guess = getGuess(correctLetters + missedLetters)
+    if guess in secretWord:
+        correctLetters = correctLetters + guess
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+        if foundAllLetters:
+            print(secretWord + '! ' + 'You win!  Nice Job!')
+            gameIsDone = True
+    else:
+        missedLetters = missedLetters + guess
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
+            print('You have run out of guesses! \nAfter ' + str(len(missedLetters)) + ' missed guesses, the word was ' + secretWord)
+            gameIsDone = True
+
+    if gameIsDone:
+        if playAgain():
+            missedLetters = ''
+            correctLetters = ''
+            secretWord = getRandomWord(words)
+            gameIsDone = False
+        else:
+            break
